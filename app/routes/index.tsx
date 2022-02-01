2f06d6c23d9cbd03ed4fc4911322f87e9f6d62b7
Noah Johnson <jnoahjohnson@gmail.com>
2022-01-31 18:29:42 -0700

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to SHOTBOX</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "hsl(181, 47%, 36%)",
+          dark: "hsl(182, 47%, 28%)",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
