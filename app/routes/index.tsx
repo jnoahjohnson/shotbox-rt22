853f5afb06dba897449b6e26c754f4fe224b8f6c
Noah Johnson <jnoahjohnson@gmail.com>
2022-02-09 15:32:17 +0000

    Update index prettier

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,11 +3,11 @@ import NavBar from "~/components/NavBar";
 
 export default function Index() {
   return (
-    <div className="px-4 isolate">
+    <div className="isolate px-4">
       <NavBar />
-      <section className="flex flex-col items-center w-full pt-32 max-w-6xl mx-auto md:items-center md:flex-row sticky top-0 z-0 -mt-20">
-        <div className="md:w-5/12 h-full flex flex-col items-start justify-center mb-16">
-          <h1 className="text-5xl font-bold mb-2 text-center md:text-left">
+      <section className="sticky top-0 z-0 mx-auto -mt-20 flex w-full max-w-6xl flex-col items-center pt-32 md:flex-row md:items-center">
+        <div className="mb-16 flex h-full flex-col items-start justify-center md:w-5/12">
+          <h1 className="mb-2 text-center text-5xl font-bold md:text-left">
             <span className="text-primary">Incredible Stories</span> Are Meant
             To Be Shared
           </h1>
@@ -21,7 +21,7 @@ export default function Index() {
           className="md:w-7/12"
         />
       </section>
-      <div className="relative -mx-4 -mt-4 z-20">
+      <div className="relative z-20 -mx-4 -mt-4">
         <svg
           width="1728"
           height="350"
@@ -35,8 +35,8 @@ export default function Index() {
           />
         </svg>
       </div>
-      <section className="relative bg-lightbg -mx-4 px-4 z-20 -mt-64">
-        <div className="text-5xl font-bold md:text-left max-w-xl mx-auto mb-32">
+      <section className="relative z-20 -mx-4 -mt-64 bg-lightbg px-4">
+        <div className="mx-auto mb-32 max-w-xl text-5xl font-bold md:text-left">
           <h1 className="text-center">
             <span className="text-primary">
               The SHOTBOX Story Capture Bundle
@@ -44,27 +44,27 @@ export default function Index() {
             has everything you need to capture your memories
           </h1>
         </div>
-        <div className="text-5xl font-bold mb-32 md:text-left max-w-xl mx-auto">
+        <div className="mx-auto mb-32 max-w-xl text-5xl font-bold md:text-left">
           <h2 className="text-center">
             Best of all,
             <span className="text-primary"> it's simple</span>
           </h2>
         </div>
-        <div className="text-5xl font-bold mb-32 md:text-left max-w-xl mx-auto">
+        <div className="mx-auto mb-32 max-w-xl text-5xl font-bold md:text-left">
           <h2 className="text-center">We promise.</h2>
         </div>
 
-        <div className="sticky bottom-1 h-screen/3 mb-12 max-w-6xl mx-auto">
+        <div className="sticky bottom-1 mx-auto mb-12 h-screen/3 max-w-6xl">
           <img
             src="/images/shotbox-main.jpg"
             alt="Shotbox on a shelf"
-            className="w-full h-full object-center object-cover rounded"
+            className="h-full w-full rounded object-cover object-center"
           />
         </div>
       </section>
       <ImageSplit />
-      <section className="relative bg-lightbg -mx-4 px-4 z-20 -mt-0 h-screen">
-        <div className="text-5xl font-bold md:text-left max-w-xl mx-auto mb-32 flex items-center h-full">
+      <section className="relative z-20 -mx-4 -mt-0 bg-lightbg px-4">
+        <div className="sticky top-0 bottom-0 mx-auto mb-32 flex h-full max-w-xl items-center text-5xl font-bold md:text-left">
           <h1 className="text-center">
             And with the perfect lighting built in, the
             <span className="text-primary"> results are incredible</span>{" "}
